abce1100c20ac49832bac6963bf1b170655868da
Sujit Kumar <me@therealsuji.tk>
2021-05-19 21:06:42 +0530

    Replaced Got with HTTP

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const app = express();
-const got = require('got');
+const http = require('http');
+const https = require('https');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/*', async (req, res) => {
+app.get('/*', (req, res) => {
     var app = "", root = "", style = "flat";
     const query = req.query;
 
+    // The Vercel application name
     if ("app" in query) {
-        var app = query["app"];
+        app = query["app"];
     } else {
-        res.status(200).redirect("https://github.com/therealsujitk/vercel-badge/blob/master/README.md");
+        res.status(302).redirect("https://github.com/therealsujitk/vercel-badge/blob/main/README.md");
         return;
     }
 
+    // The application path
     if ("root" in query) {
-        var root = query["root"];
+        root = query["root"];
     }
 
+    // The style of the badge
     if ("style" in query) {
         if (query["style"] == "flat-square") {
             style = "flat-square";
@@ -27,23 +31,51 @@ app.get('/*', async (req, res) => {
         }
     }
 
-    var statusCode = 200;
+    const url = app + ".vercel.app/" + root;
+    const handleRequest = (statusCode) => {
+        var status = "deployed";
+    
+        if (statusCode <= 599 && statusCode >= 500) {
+            // 500 - 599 -> Server Errors
+            status = "failed";
+        } else if (statusCode <= 499 && statusCode >= 400) {
+            // 400 - 499 -> Client Errors
+            status = "failed";
+        } else if (statusCode <= 399 && statusCode >= 300) {
+            // 300 - 399 -> Redirects
+        }
+    
+        // 200 - 299 -> Successful Responses
+        // 100 - 199 -> Informational Responses
+    
+        res.status(200).sendFile(__dirname + "/public/assets/images/vercel-" + status + "-" + style + ".svg");
+    }
 
     try {
-        const url = "http://" + app + ".vercel.app/" + root;
-        const response = await got(url);
-        statusCode = response.statusCode;
-    } catch (error) {
-        statusCode = 500;
+        http.get("http://" + url, (response) => {
+            var statusCode = response.statusCode;
+
+            // 308 -> Permanent Redirect
+            // This could mean HTTP is being redirected to HTTPS
+            if (statusCode == 308) {
+                https.get("https://" + url, (response) => {
+                    statusCode = response.statusCode;
+                    handleRequest(statusCode);
+                }).on('error', (err) => {
+                    // The HTTPS page wasn't found
+                    handleRequest(statusCode);
+                }); 
+            } else {
+                handleRequest(statusCode);
+            }
+        }).on('error', (err) => {
+            // For some reason HTTP encountered an error
+            res.status(500).send(err);
+        });
+    } catch {
+        // Invalid Application Name
+        handleRequest(404);
     }
-
-    var status = "deployed";
-
-    if (statusCode == 500 || statusCode == 404) {
-        status = "failed";
-    }
-
-    res.status(200).sendFile(__dirname + "/public/assets/images/vercel-" + status + "-" + style + ".svg");
 });
 
 module.exports = app;
